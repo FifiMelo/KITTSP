708913b67e2b1b47a76e9be8f06e0b634860aaff
FifiMelo <felipe336336@gmail.com>
2023-12-22 15:31:34 +0100

    adding simple constraints

--- kittsp.py
+++ kittsp.py
@@ -1,12 +1,17 @@
 import cplex
 
-def get_pairs(list):
+
+def get_variables_of_node(node_name, tour_index, cpx_object):
     """
-    This function yields a list of all possible pairs of elements from the given list
+    This function returns a list of variables corresponding to the given node, and given tour index
     """
-    for i in range(len(list)):
-        for j in range(i + 1, len(list)):
-            yield [list[i], list[j]]
+    all_variables = cpx_object.variables.get_names()
+    selected_variables = []
+    for variable in all_variables:
+        node1, node2, tour = variable.split("-")
+        if (node1 == node_name or node2 == node_name) and tour == tour_index:
+            selected_variables.append(variable)
+    return selected_variables
 
 
 def get_input(filename, cpx_object):
@@ -42,11 +47,34 @@ def get_input(filename, cpx_object):
         # adding varaibles
         for i in range(M):
             node1, node2, cost = file.readline().split()
+            variable_names = [f"{node1}-{node2}-{tour_index}" for tour_index in range(K)]
             cpx_object.variables.add(
                 obj = [float(cost)] * K,  
                 types = [cpx_object.variables.type.binary] * K, 
-                names = [f"{node1}-{node2}-{j}" for j in range(K)]
+                names = [f"{node1}-{node2}-{tour_index}" for tour_index in range(K)]
                 )
+            
+            # adding constraints that none of the edges can be in more that one tour
+            lin_expr = cplex.SparsePair(
+                ind = variable_names,
+                val = [1] * len(variable_names))
+            cpx_object.linear_constraints.add(lin_expr = [lin_expr],
+                                                   senses=["L"], rhs = [2])
+            
+            
+        # adding simple constraints
+        # that in each tour in each node exactly two edges must be selected
+        for node in Nodes:
+            for tour_index in range(K):
+                variables = get_variables_of_node(node, tour_index, cpx_object)
+                lin_expr = cplex.SparsePair(
+                    ind = variables,
+                    val = [1] * len(variables)
+                    )
+                cpx_object.linear_constraints.add(lin_expr = [lin_expr],
+                                                   senses=["E"], rhs = [2])
+        
+        
         
         
 
@@ -60,6 +88,8 @@ def main():
     cpx.parameters.threads.set(1)
     cpx.objective.set_sense(cpx.objective.sense.minimize)
     get_input("./data_for_kittsp.txt", cpx)
+
+    print(cpx.linear_constraints.get_num())
 
 
 
